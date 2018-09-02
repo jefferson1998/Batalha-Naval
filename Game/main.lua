fac55748b96494a38ad029046f600fd229f31113
jefferson1998 <jefferson05061998@gmail.com>
2018-09-02 15:24:44 -0300

    Modificação na função inserir navio!

--- Game/main.lua
+++ Game/main.lua
@@ -55,22 +55,18 @@ local mapaJogador2 = {
 
 ----- Funções -----------
 function preencherTabuleiro(tabuleiro)
-
 	for i=1,10 do
 		for j=1,10 do
 			tabuleiro[i][j] = 0
 		end
 	end
-
 end
 
 function viewTabuleiro(tabuleiro)
 	local strTabuleiro = ""
 	for i=1, #tabuleiro do
 		for j=1, #tabuleiro[i] do
-		strTabuleiro = strTabuleiro .. tabuleiro[i][j]
-
-		
+			strTabuleiro = strTabuleiro .. tabuleiro[i][j]
 		end
 			strTabuleiro = strTabuleiro .. "\n"
 	end
@@ -79,39 +75,55 @@ function viewTabuleiro(tabuleiro)
 
 end 
 
-function inserirNavio(navio, linha, coluna, tabuleiro)
-local posicaoOk = true
+function inserirNavio(jogador, linha, coluna, opcao, orientacao, tamanhoTotalDosBarcos, naviosRestantes)
+local posicaoOk = false
 
+for k,v in pairs(jogador.totalNavios) do
+		
+	if opcao == v.tamanho then
+		opcao = v
+		posicaoOk = true
+		opcao.orientacao = orientacao
 
-	if navio.orientacao == "vertical" then
-		for i = navio.tamanho, 1, -1 do
-			if tabuleiro[linha + (i - navio.tamanho)][coluna] ~= 0 then
-				posicaoOk = false
+		if opcao.orientacao == "vertical" then
+			for i = opcao.tamanho, 1, -1 do
+				if jogador.tabuleiro[linha + (i - opcao.tamanho)][coluna] ~= 0 then
+					posicaoOk = false
+				end
 			end
-		end
-	else 
-		for i=0, navio.tamanho -1, 1 do
-			if tabuleiro[linha][coluna + i] ~=  0 then
-				posicaoOk = false
+		else 
+			for i=0, opcao.tamanho -1, 1 do
+				if jogador.tabuleiro[linha][coluna + i] ~=  0 then
+					posicaoOk = false
+				end
 			end
 		end
 	end
+end
 
 	if posicaoOk == true then
-		if navio.orientacao == "vertical" then
-			for i = navio.tamanho, 1, -1 do
-				tabuleiro[linha + (i - navio.tamanho)][coluna] = navio.tamanho
+		if opcao.orientacao == "vertical" then
+			for i = opcao.tamanho, 1, -1 do
+				jogador.tabuleiro[linha + (i - navio.tamanho)][coluna] = opcao.tamanho
 			end
+			naviosRestantes = naviosRestantes - 1
+			tamanhoTotalDosBarcos = tamanhoTotalDosBarcos + opcao.tamanho
+			print("\nNavio inserido!\n")
+			return tamanhoTotalDosBarcos, naviosRestantes
 		else
-			for i=0, navio.tamanho -1, 1 do
-					tabuleiro[linha][coluna + i] = navio.tamanho
+			for i=0, opcao.tamanho -1, 1 do
+				jogador.tabuleiro[linha][coluna + i] = opcao.tamanho
 			end
+			naviosRestantes = naviosRestantes - 1
+			tamanhoTotalDosBarcos = tamanhoTotalDosBarcos + opcao.tamanho
+			print("\nNavio inserido!\n")
+			return tamanhoTotalDosBarcos, naviosRestantes
 		end
-		print("Navio inserido!")
+
 	else
-		return false
+		return print("\nPosicao nao disponivel!\n")
 	end
-	
+
 end
 
 function atirarNoTabuleiro(tabuleiro, mapa, linha, coluna, pontuacao)
@@ -153,12 +165,10 @@ local jogador2 = {
 	totalNavios = navios
 }
 
-
 preencherTabuleiro(jogador1.tabuleiro)
 preencherTabuleiro(jogador2.tabuleiro)
 
 --------- Inserir Navios do Jogador 1 --------
-
 
 print("--------- Player 1  inserir Navios -----\n\n")
 local naviosRestantes = 4
@@ -168,87 +178,52 @@ repeat
 local opcao, orientacao
 local x, y
 
-print("Digite o tamanho do navio 2, 3, 4, 5 ou 0 para sair")
-opcao = io.read("*n")
+	print("Digite o tamanho do navio \n 2. Corveta \n 3. Fragata \n 4. PortaAvioes\n 5. Cruzador\n 0 para sair")
+	opcao = io.read("*n")
 
-if opcao == 0 then return end
+	if opcao == 0 then return end
 
-print("Posição X no tabuleiro")
-x = io.read("*n")
+	print("Posição X no tabuleiro")
+	x = io.read("*n")
 
-print("Posição Y no tabuleiro")
-y = io.read("*n")
+	print("Posição Y no tabuleiro")
+	y = io.read("*n")
 
-print("vertical || horizontal")
-io.read()
-orientacao = io.read()
+	print("vertical || horizontal")
+	io.read()
+	orientacao = io.read()
 
-for k,v in pairs(jogador1.totalNavios) do
-	if opcao == v.tamanho then
-		opcao = v
-
-		opcao. orientacao = orientacao
-
-		if inserirNavio(opcao, x, y, jogador1.tabuleiro) == false then
-			print("Posição não disponível!\n")
-		else
-			naviosRestantes = naviosRestantes - 1
-			tamanhoTotalDosBarcos1 = tamanhoTotalDosBarcos1 + v.tamanho
-			print(k .. " inserido\n")
-		end
-	end
-end
-
-print(viewTabuleiro(jogador1.tabuleiro))
-
-if naviosRestantes == 0 then
-	print("Navios Inseridos!")
-end
+ 	tamanhoTotalDosBarcos1, naviosRestantes = inserirNavio(jogador1, x, y, opcao, orientacao, tamanhoTotalDosBarcos1, naviosRestantes)
+ 	
+	print(viewTabuleiro(jogador1.tabuleiro))
 
 until (opcao == 0 or naviosRestantes == 0)
 
 ----- Navios do Jogador 2 ----
-local tamanhoTotalDosBarcos2 = 0
 print("\n\n--------- Player 2  inserir Navios ----- \n\n")
 naviosRestantes = 4
+tamanhoTotalDosBarcos2 = 0
+
 repeat
 
-print("Digite o tamanho do navio 2, 3, 4, 5 ou 0 para sair")
-opcao = io.read("*n")
+	print("Digite o tamanho do navio \n 2. Corveta \n 3. Fragata \n 4. PortaAvioes\n 5. Cruzador\n 0 para sair")
+	opcao = io.read("*n")
 
-if opcao == 0 then return end
+	if opcao == 0 then return end
 
-print("Posição X no tabuleiro")
-x = io.read("*n")
+	print("Posição X no tabuleiro")
+	x = io.read("*n")
 
-print("Posição Y no tabuleiro")
-y = io.read("*n")
+	print("Posição Y no tabuleiro")
+	y = io.read("*n")
 
-print("vertical || horizontal")
-io.read()
-orientacao = io.read()
-	
-for k,v in pairs(jogador2.totalNavios) do
-	if opcao == v.tamanho then
-		opcao = v
+	print("vertical || horizontal")
+	io.read()
+	orientacao = io.read()
+		
+	tamanhoTotalDosBarcos2, naviosRestantes = inserirNavio(jogador2, x, y, opcao, orientacao, tamanhoTotalDosBarcos2, naviosRestantes)
 
-		opcao. orientacao = orientacao
-
-		if inserirNavio(opcao, x, y, jogador2.tabuleiro) == false then
-			print("Posição não disponível!")
-		else
-			naviosRestantes = naviosRestantes - 1
-			tamanhoTotalDosBarcos2 = tamanhoTotalDosBarcos2 + v.tamanho
-			print(k .. " inserido\n")
-		end
-	end
-end
-
-print(viewTabuleiro(jogador2.tabuleiro))
-
-if naviosRestantes == 0 then
-	print("Navios Inseridos!")
-end
+	print(viewTabuleiro(jogador2.tabuleiro))
 
 until (opcao == 0 or naviosRestantes == 0)
 
@@ -256,58 +231,8 @@ local vezPlayer1 = false
 local vezPlayer2 = false
 
 repeat
-
-print("\n\n----Jogador 1 é sua vez -----\n\n" .. viewTabuleiro(jogador2.mapa))
-
-print("\nPosição X no tabuleiro")
-x = io.read("*n")
-
-print("Posição Y no tabuleiro")
-y = io.read("*n")
-
-vezPlayer1, jogador1.pontuacao = atirarNoTabuleiro(tabuleiro1, jogador2.mapa, x, y, jogador1.pontuacao)
-
-if jogador1.pontuacao == tamanhoTotalDosBarcos1 then 
-		print("Vencedor " .. jogador1.nome)
-return end
-
-print("\n" .. viewTabuleiro(jogador2.mapa))
-
-while vezPlayer1 == true do
-
-	print("Jogue mais uma vez! \n".. viewTabuleiro(jogador2.mapa))
-
-	print("\nPosição X no tabuleiro")	
-	x = io.read("*n")
-
-	print("Posição Y no tabuleiro")
-	y = io.read("*n")
-
-	vezPlayer1, jogador1.pontuacao = atirarNoTabuleiro(tabuleiro1, jogador2.mapa, x, y, jogador1.pontuacao)
-
-	if jogador1.pontuacao == tamanhoTotalDosBarcos1 then 
-		print("Vencedor " .. jogador1.nome)
-	return end
-end
-
-print("\n\n----Jogador 2 é sua vez -----\n\n" .. viewTabuleiro(jogador1.mapa))
-
-print("\nPosição X no tabuleiro")
-x = io.read("*n")
-
-print("Posição Y no tabuleiro")
-y = io.read("*n")
-
-vezPlayer2, jogador2.pontuacao = atirarNoTabuleiro(tabuleiro, jogador1.mapa, x, y, jogador2.pontuacao)
-
-if jogador2.pontuacao == tamanhoTotalDosBarcos2 then
-		print("Vencedor " .. jogador2.nome)
-return end
-
-print("\n" .. viewTabuleiro(jogador1.mapa))
-
-while vezPlayer2 == true do
-	print("Jogue mais uma vez! \n".. viewTabuleiro(jogador1.mapa))
+ ----------- GAME ------------
+	print("\n\n----Jogador 1 é sua vez -----\n\n" .. viewTabuleiro(jogador2.mapa))
 
 	print("\nPosição X no tabuleiro")
 	x = io.read("*n")
@@ -315,11 +240,56 @@ while vezPlayer2 == true do
 	print("Posição Y no tabuleiro")
 	y = io.read("*n")
 
-	vezPlayer2, jogador2.pontuacao = atirarNoTabuleiro(tabuleiro, jogador1.mapa, x, y, jogador2.pontuacao)
-	
-	if jogador2.pontuacao == tamanhoTotalDosBarcos2 then
-		print("Vencedor " .. jogador2.nome)
-	return end
-end
+	vezPlayer1, jogador1.pontuacao = atirarNoTabuleiro(tabuleiroJogador2, jogador2.mapa, x, y, jogador1.pontuacao)
+
+	print("\n" .. viewTabuleiro(jogador2.mapa))
+
+	while vezPlayer1 == true do
+
+		if jogador1.pontuacao == tamanhoTotalDosBarcos1 then 
+			print("Vencedor " .. jogador1.nome)
+		return end
+
+		print("Jogue mais uma vez! \n".. viewTabuleiro(jogador2.mapa))
+
+		print("\nPosição X no tabuleiro")	
+		x = io.read("*n")
+
+		print("Posição Y no tabuleiro")
+		y = io.read("*n")
+
+		vezPlayer1, jogador1.pontuacao = atirarNoTabuleiro(tabuleiroJogador2, jogador2.mapa, x, y, jogador1.pontuacao)
+
+	end
+
+	print("\n\n----Jogador 2 é sua vez -----\n\n" .. viewTabuleiro(jogador1.mapa))
+
+	print("\nPosição X no tabuleiro")
+	x = io.read("*n")
+
+	print("Posição Y no tabuleiro")
+	y = io.read("*n")
+
+	vezPlayer2, jogador2.pontuacao = atirarNoTabuleiro(tabuleiroJogador1, jogador1.mapa, x, y, jogador2.pontuacao)
+
+	print("\n" .. viewTabuleiro(jogador1.mapa))
+
+	while vezPlayer2 == true do
+
+		if jogador2.pontuacao == tamanhoTotalDosBarcos2 then
+			print("Vencedor " .. jogador2.nome)
+		return end
+
+		print("Jogue mais uma vez! \n".. viewTabuleiro(jogador1.mapa))
+
+		print("\nPosição X no tabuleiro")
+		x = io.read("*n")
+
+		print("Posição Y no tabuleiro")
+		y = io.read("*n")
+
+		vezPlayer2, jogador2.pontuacao = atirarNoTabuleiro(tabuleiroJogador1, jogador1.mapa, x, y, jogador2.pontuacao)
+
+	end
 
 until jogador1.pontuacao == tamanhoTotalDosBarcos1 or jogador2.pontuacao == tamanhoTotalDosBarcos2
